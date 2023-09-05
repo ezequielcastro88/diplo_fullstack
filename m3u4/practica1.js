71468c92ec028da8202af004ef01b32f859a02c4
Ezequiel Castro <ezequiel.r.castro88@gmail.com>
2023-09-05 11:44:44 -0300

    m3u4

---
+++ m3u4/practica1.js
@@ -0,0 +1,16 @@
+const cuadrado1 = document.getElementById("cuadrado1");
+const cuadrado2 = document.getElementById("cuadrado2");
+const cuadrado3 = document.getElementById("cuadrado3");
+
+
+cuadrado1.addEventListener("click", function() {
+    cuadrado1.style.backgroundColor = '#9999';
+});
+
+cuadrado2.addEventListener("click", function() {
+    cuadrado2.style.backgroundColor = '#2899';
+});
+
+cuadrado3.addEventListener("click", function() {
+    cuadrado3.style.backgroundColor = '#1688';
+});
